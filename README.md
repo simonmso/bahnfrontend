2398300add27aa28d3c162decf300d4e3fe4941d
Simon <simonmissoula@gmail.com>
2023-01-04 12:09:16 -0500

    getting ready to branch for html clock

---
+++ README.md
@@ -0,0 +1,8 @@
+This will at some point have more details on how to start this project
+
+for now:
+
+`npm install`
+`npm run build`
+
+then open `./js/dist/index.html` in a browser
--- js/src/dummy.js
+++ js/src/dummy.js
@@ -5,7 +5,10 @@ const withs = [
   [undefined, 5], [18, 20], [38, 41], [44, 45], [55, 57], [10, 12], [22, 24], [33, undefined],
 ];
 const add = [0, 0, 0, 0, 0, 1, 1, 1];
-const names = ["Dresden-Neustadt", "Ruhland", "Ortrand", "Lampertswalde", "Großenhain Cottb Bf", "Priestewitz", "Weinböhla Hp", "Coswig(b Dresden)"];
+const names = [
+  "Dresden-Neustadt", "Ruhland", "Ortrand", "Lampertswalde",
+  "Großenhain Cottb Bf", "Priestewitz", "Weinböhla Hp", "Coswig(b Dresden)"
+];
 
 const stops = names.map((n, i) => ({
   name: n,
