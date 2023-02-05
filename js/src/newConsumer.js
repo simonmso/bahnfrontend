4bba8c9149ddcf82e75472278be9e6b1483e48b7
Simon Silverstein <simonmissoula@gmail.com>
2023-02-05 12:08:33 -0500

    fixed bug where stops old stops weren't deleted

--- js/src/newConsumer.js
+++ js/src/newConsumer.js
@@ -1,4 +1,5 @@
 import Stop from './Stop';
+import { removeStop } from './dom/stopFns';
 
 const getJourney = () => {
     const url = '/api/journey';
@@ -7,13 +8,23 @@ const getJourney = () => {
         .then((stops) => stops.map((s) => new Stop(s)));
 };
 
-export const updateStops = async (oldStops) => {
-    const newStops = await getJourney();
-    return newStops.map((newStop) => {
-        const oldStop = oldStops?.find((o) => o.id === newStop.id);
-        const ret = oldStop ? oldStop.with(newStop) : newStop;
-        return ret;
+export const updateStops = async (stops) => {
+    const oldStops = stops || [];
+    const journey = await getJourney();
+    const newStops = journey.map((newStop) => {
+        const oldIdx = oldStops.findIndex((o) => o.id === newStop.id);
+        if (oldIdx >= 0) {
+            const oldStop = oldStops.splice(oldIdx, 1)[0];
+            return oldStop.with(newStop);
+        }
+        return newStop;
     });
+
+    // delete the dots and curves for any stops that
+    // are no longer included
+    oldStops.forEach(removeStop);
+
+    return newStops;
 };
 
 export default getJourney;
